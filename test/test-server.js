fedeccd43cdfa95b6823e16232d8b2b2a9ec371e
Amy <amy.speed.henley@gmail.com>
2019-04-26 15:16:23 -0500

    Level GET tests passing

--- test/test-server.js
+++ test/test-server.js
@@ -29,33 +29,40 @@ describe('Protect levels endpoint', function() {
         return runServer(TEST_DATABASE_URL);
     });
 
-    // beforeEach(function() {
-    //     return User.hashPassword(password)
-    //         .then(password => 
-    //             User.create({
-    //                 username,
-    //                 password,
-    //                 firstName,
-    //                 lastName
-    //             })
-    //     );
-    // });
+    beforeEach(function() {
+        return User.hashPassword(password)
+            .then(password => 
+                User.create({
+                    username,
+                    password,
+                    firstName,
+                    lastName
+                })
+        );
+    });
 
-    // beforeEach(function() {
-    //     return Level.create({
-    //         level,
-    //         instructions,
-    //         questions
-    //     })
-    // });
+    beforeEach(function() {
+        return Level.create([
+            {
+                level,
+                instructions,
+                questions,
+            },
+            {
+                level,
+                instructions,
+                questions,
+            }
+        ])
+    });
 
-    // afterEach(function() {
-    //     return User.deleteOne({});
-    // });
+    afterEach(function() {
+        return User.deleteOne({});
+    });
 
-    // afterEach(function() {
-    //     return Level.deleteMany({})
-    // });
+    afterEach(function() {
+        return Level.deleteMany({})
+    });
 
     after(function() {
         return closeServer();
@@ -68,6 +75,81 @@ describe('Protect levels endpoint', function() {
                 .get('/api')
                 .then(function(res) {
                     expect(res).to.have.status(200);
+                });
+        });
+    });
+
+    describe('/api/levels/*', function() {
+
+        const authToken = jwt.sign(
+            {
+                user: {
+                    username,
+                    firstName,
+                    lastName
+                }
+            },
+            JWT_SECRET,
+            {
+                algorithm: 'HS256',
+                subject: username,
+                expiresIn: '7d'
+            }
+        );
+
+        it('should GET all Levels', function() {
+            return chai
+                .request(app)
+                .get('/api/levels')
+                .set('Authorization', `Bearer ${authToken}`)
+                .then(function(res) {
+                    expect(res).to.have.status(200);
+                    expect(res).to.be.json;
+                    expect(res.body).to.be.an('array');
+                    res.body.forEach(function(level) {
+                        expect(level).to.be.an('object');
+                        expect(level).to.contain.keys(
+                            '__v',
+                            '_id',
+                            'level',
+                            'instructions',
+                            'questions'
+                        );
+                    })
+                });
+        });
+
+        it('should GET one level by id', function() {
+            return Level
+                .findOne()
+                .then(function(level) {
+                    let id = level._id;
+
+                    return chai 
+                        .request(app) 
+                        .get(`/api/levels/${id}`)
+                        .set('Authorization', `Bearer ${authToken}`)
+                        .then(function(res) {
+                            expect(res).to.have.status(200);
+                            expect(res).to.be.json;
+                            expect(res.body).to.be.an('object');
+                            expect(res.body).to.contain.keys(
+                                '__v',
+                                '_id',
+                                'level',
+                                'instructions',
+                                'questions'
+                            );
+                            expect(res.body.questions).to.be.an('array');
+                            res.body.questions.forEach(function(question) {
+                                expect(question).to.be.an('object');
+                                expect(question).to.contain.keys(
+                                    'questionNum',
+                                    'question',
+                                    'answer'
+                                );
+                            });
+                        });
                 });
         });
     });
