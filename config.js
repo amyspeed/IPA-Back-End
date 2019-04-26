ba3795168e2d3fbf4326fbaaf224706335ae2d7b
Amy <amy.speed.henley@gmail.com>
2019-04-26 17:56:12 -0500

    Client Heroku address

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 'use strict';
 
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://AmySpeed:Password123@ds237717.mlab.com:37717/ipa-app';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-ditties-app';
-exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-ipa-app';
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'https://pacific-basin-65264.herokuapp.com/';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
