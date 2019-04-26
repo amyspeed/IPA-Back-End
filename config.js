a92f4235dcfa28bbed44acd3eba02a4f24a572f1
Amy <amy.speed.henley@gmail.com>
2019-04-26 16:09:08 -0500

    Local test DB URL

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://AmySpeed:Password123@ds237717.mlab.com:37717/ipa-app';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://AmySpeed:Password123@ds237717.mlab.com:37717/ipa-app';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-ditties-app';
 exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
