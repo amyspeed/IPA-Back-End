ddaacf867b2f57e484052300cabcc11b1af87b81
Amy Speed <amy.speed.henley@gmail.com>
2020-05-20 16:26:26 -0500

    data migrated from mlab to atlas

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 exports.DATABASE_URL = 
     process.env.DATABASE_URL ||
     global.DATABASE_URL ||
-    'mongodb://AmySpeed:Password123@ds237717.mlab.com:37717/ipa-app';
+    'mongodb+srv://AmySpeed:Password123@cluster0-ndw5d.mongodb.net/ipa-learning-app?retryWrites=true&w=majority';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-ipa-app';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
